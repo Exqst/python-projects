a0ed5f7e2c646d805cf126324e9c999d28c28f3a
Xqst <gatdula.raymondcp@gmail.com>
2022-06-26 19:47:09 +0800

    Create UserInputs.py
    User inputs with a basic if else conditions

---
+++ Basics-Python/UserInputs.py
@@ -0,0 +1,9 @@
+print("Hello and welcome to the program")
+name = input("May I please know your name?: ")
+print("Welcome " + name+" this is the basics of python")
+age = int(input("May I please know your age?: "))
+if(age > 18):
+    print("You are still yound and can still pick up to learn python, good job!")
+
+else:
+    print("You are young and eager to study python, good job!")
