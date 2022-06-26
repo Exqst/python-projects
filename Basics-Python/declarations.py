ab92e9d65f3bd123363d1b3ddeaeb2f826b8c8f7
Xqst <gatdula.raymondcp@gmail.com>
2022-06-26 19:29:15 +0800

    Create declarations.py
    Declaration and how to print different data types

---
+++ Basics-Python/declarations.py
@@ -0,0 +1,4 @@
+name = "Monde"
+age = 23
+print("My name is " + name) ##If you want to concatenate string you will use + 
+print("I am ", age, "yrs old")##If you want to concatenate int or any numerical value you will use ,
