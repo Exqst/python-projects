d0a099ccb044ac4b2b67aaeeceab8599dbc6aa33
Xqst <gatdula.raymondcp@gmail.com>
2022-06-26 19:40:29 +0800

    Update declarations.py
    Added comments and defined the data type

--- Basics-Python/declarations.py
+++ Basics-Python/declarations.py
@@ -1,4 +1,4 @@
 name = "Monde"
-age = 23
+age = int(23) ##This is how to change the data type of a value, tho python will automatic set this. This can be good practice for a when working in a group.
 print("My name is " + name) ##If you want to concatenate string you will use + 
 print("I am ", age, "yrs old")##If you want to concatenate int or any numerical value you will use ,
